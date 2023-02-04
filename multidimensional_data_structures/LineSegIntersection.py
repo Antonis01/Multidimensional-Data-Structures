0dcdaa707b69a7cc28a8245ce1fd25bc6502587e
Antonis Anastasiou <antonisanastasiou2012@gmail.com>
2023-02-04 23:51:07 +0200

    bug fixes and completion
    finished with line segment intersection, fixed some bugs,
    added comments,
    showing the plot of the lines

--- multidimensional_data_structures/LineSegIntersection.py
+++ multidimensional_data_structures/LineSegIntersection.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 
 class Point:
@@ -7,51 +8,59 @@ class Point:
         self.y = y
 
 
-def on_segment(p, q, r):
-    if max(p.x, r.x) >= q.x >= min(p.x, r.x) and max(p.y, r.y) >= q.y >= min(p.y, r.y):
+# The function checks if point q1 lies online segment p1,r
+# based on the 3 collinear points
+def on_segment(p1, q1, r):
+    if max(p1.x, r.x) >= q1.x >= min(p1.x, r.x) and max(p1.y, r.y) >= q1.y >= min(p1.y, r.y):
         return True
     return False
 
 
-def cross_product(p1, p2, p3):
-    return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x)
-
-
-def orientation(p, q, r):
-    val = cross_product(p, q, r)
+# finding the orientation of the 3 points
+def orientation(a, b, r):
+    val = (b.y - a.y) * (r.x - b.x) - (b.x - a.x) * (r.y - b.y)
     if val == 0:
-        return 0  # collinear
+        # collinear
+        return 0
     elif val > 0:
-        return 1  # clockwise
+        # clockwise
+        return 1
     else:
-        return 2  # counterclockwise
+        # counterclockwise
+        return 2
 
 
 def do_intersect(p1, q1, p2, q2):
-    r = Point(q2.x - p1.x, q2.y - p1.y)
-    s = Point(q1.x - p2.x, q1.y - p2.y)
-    val = cross_product(p1, q2, q1)
-    if val == 0:
-        return False
-    t = cross_product(p2, q2, q1) / val
-    if t < 0 or t > 1:
-        return False
-    u = cross_product(p1, q2, p2) / val
-    if u < 0 or u > 1:
-        return False
-    return True
+    # We find the orientation using the p and q of each point
+    o1 = orientation(p1, q1, p2)
+    o2 = orientation(p1, q1, q2)
+    o3 = orientation(p2, q2, p1)
+    o4 = orientation(p2, q2, q1)
 
+    # General case
+    if o1 != o2 and o3 != o4:
+        return True
 
-def line_segment_intersection(x):
-    n = len(x)
-    for i in range(n):
-        for j in range(i + 1, n):
-            p1 = x[i]
-            q1 = x[j]
-            p2 = x[(i + 1) % n]
-            q2 = x[(j + 1) % n]
-            if do_intersect(p1, q1, p2, q2):
-                return True
+    # p1,q1,p2 are collinear
+    # p2 lies on segment p1,q1
+    if o1 == 0 and on_segment(p1, p2, q1):
+        return True
+
+    # p1,q1,q2 are collinear
+    # q2 lies on segment p1,q1
+    if o2 == 0 and on_segment(p1, q2, q1):
+        return True
+
+    # p2,q2,p1 are collinear
+    # p1 lies on segment p2,q2
+    if o3 == 0 and on_segment(p2, q1, q2):
+        return True
+
+    # p2,q2,q1 are collinear
+    # p1 lies on segment p2,q2
+    if o4 == 0 and on_segment(p2, q1, q2):
+        return True
+
     return False
 
 
@@ -61,18 +70,29 @@ with open('coordinates.csv', mode='r', encoding='utf-8-sig') as file:
     reader = csv.reader(file)
 
     # p and q are the two points of a line
-
     # Insert the coordinates of each line
     for row in reader:
+        # Adds the columns to x and y of p and q
+        # So now we have p.x,p.y and q.x,q.y
         p = Point(float(row[0]), float(row[1]))
         q = Point(float(row[2]), float(row[3]))
+        # Add those two points in an array
         points.append(p)
         points.append(q)
 
+
 for i in range(0, len(points) - 1, 2):
-    for j in range(0, len(points) - 1, 2):
+    # we start from i + 2 in the second for loop because we don't want to check two times for every point
+    # because the array is 1D
+    for j in range(i + 2, len(points) - 1, 2):
         if do_intersect(points[i], points[i + 1], points[j], points[j + 1]):
             print("Lines {},{}-{},{} and {},{}-{},{} intersect".format(int(points[i].x), int(points[i].y),
                                                                        int(points[i + 1].x), int(points[i + 1].y),
                                                                        int(points[j].x), int(points[j].y),
                                                                        int(points[j + 1].x), int(points[j + 1].y)))
+
+            # we make and show the plot for the lines that intersect 
+            plt.figure(figsize=(5, 5))
+            plt.plot((int(points[i].x), int(points[i + 1].x)), (int(points[i].y), int(points[i + 1].y)), '.r--')
+            plt.plot((int(points[j].x), int(points[j + 1].x)), (int(points[j].y), int(points[j + 1].y)), '.b--')
+            plt.show()
